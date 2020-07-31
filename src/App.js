83b3c033871791ebdc8bf6a11152755e021eb629
mervecakici <smervecakici@gmail.com>
2020-07-31 21:17:21 +0200

    test script is deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import MainComponent from "./Components/MainComponent"
-import useScript from './hooks/useScript';
 
 function App() {
-  useScript('https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js')
-  return (
-   
-      <MainComponent />
-    
-    
-  );
+  return <MainComponent />
 }
 
 export default App;
