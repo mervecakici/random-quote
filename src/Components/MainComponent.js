e343957a0d85069293d3d4204625b2523e1bdd7e
mervecakici <smervecakici@gmail.com>
2020-07-31 16:22:51 +0200

    animations added, design arranged again.

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { getRandomNumber } from '../Utils/MathUtils.js'
 
 import "./MainComponent.css";
-import { TwitterShareButton, TwitterIcon,  FacebookShareButton, WhatsappShareButton, WhatsappIcon,  FacebookIcon} from "react-share";
+import { TwitterShareButton, TwitterIcon,  WhatsappShareButton, WhatsappIcon, } from "react-share";
 
 
-var COLORS = ["#F3F781", "#81DAF5", "#04B4AE", "#FACC2E", "#FA8258", "#F5BCA9", "#D8CEF6", "#00FFBF", "#FA5858", "#FF00FF", "#0080FF"]
+var COLORS = ["#81DAF5", "#04B4AE", "#FACC2E", "#FA8258", "#F5BCA9", "#D8CEF6", "#00FFBF", "#FA5858", "#FF00FF", "#0080FF"]
 
 class MainComponent extends React.Component {
     constructor(props){
@@ -60,11 +60,11 @@ class MainComponent extends React.Component {
     }
  
     render(){
-        const { quoteDataArray, quoteIndex, colorIndex,quotePrevArray } = this.state
+        const { quoteDataArray, quoteIndex, colorIndex} = this.state
         const { text, author } = quoteDataArray[quoteIndex]
         const authorString = author === null ? "Unknown author" : author
-        console.log("mrev",quoteDataArray[quoteIndex])
-        console.log("abc",quotePrevArray)
+        
+        
 
         return(
             <div id="main-container" style={{backgroundColor: COLORS[colorIndex]}}>
@@ -73,9 +73,9 @@ class MainComponent extends React.Component {
                     <p id="author" style={{color: COLORS[colorIndex]}}>{`-${authorString}`}</p>
                     <div class="buttons">
                         <a href="twitter.com/intent/tweet" class="share-buttons" id="tweet-quote" ><TwitterShareButton url={`"${text}" -${authorString} #quoteoftheday`}><TwitterIcon size={32} round={true} /></TwitterShareButton></a>
-                        <a class="share-buttons" id="whatsapp-quote" ><WhatsappShareButton url={`"${text}" -${authorString}`}><WhatsappIcon size={32} round={true}/></WhatsappShareButton></a>
-                        <button id="back-button" onClick={this.handlePreviousQuote} disabled={this.state.quotePrevArray.length<=0} style={{backgroundColor: COLORS[colorIndex]}}>Previus</button>
-                        <button id="new-quote" onClick={this.handleChangeQuote} style={{backgroundColor: COLORS[colorIndex]}}>New Quote</button>
+                        <a href="web.whatsapp.com"class="share-buttons" id="whatsapp-quote" ><WhatsappShareButton url={`"${text}" -${authorString}`}><WhatsappIcon size={32} round={true}/></WhatsappShareButton></a>
+                        <button id="back-button" onClick={this.handlePreviousQuote} disabled={this.state.quotePrevArray.length<=0} style={{backgroundColor: COLORS[colorIndex], borderColor: COLORS[colorIndex] }}>Previus</button>
+                        <button id="new-quote" onClick={this.handleChangeQuote} style={{backgroundColor: COLORS[colorIndex], borderColor: COLORS[colorIndex]}} >New Quote</button>
                     </div>
                 </div>
             </div>
